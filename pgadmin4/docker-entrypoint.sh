69c20d02bda6b8be72366044b78d1717ff5f8ab5
Michael J. Stealey <michael.j.stealey@gmail.com>
2017-03-16 21:12:11 -0400

    persist pgAdmin4 settings

--- pgadmin4/docker-entrypoint.sh
+++ pgadmin4/docker-entrypoint.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 if [[ "$1" = 'pgadmin' ]]; then
-    exec python /pgadmin4/web/pgAdmin4.py
+    useradd -d /var/pgadmin --system -s /usr/sbin/nologin -c "pgAdmin System Account" -U pgadmin
+    usermod -u ${PGADMIN_UID} pgadmin
+    groupmod -g ${PGADMIN_GID} pgadmin
+    chown -R ${PGADMIN_UID}:${PGADMIN_GID} /pgadmin4 /var/pgadmin
+    su --shell /bin/bash -c "python /pgadmin4/web/pgAdmin4.py" pgadmin
 else
     exec "$@"
 fi
