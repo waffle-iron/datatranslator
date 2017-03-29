a1d6fad76db9fc8cb3e2d79d5cc2a52654ec58de
Michael J. Stealey <michael.j.stealey@gmail.com>
2017-03-29 12:46:09 -0400

    [#19] script to run api server in docker

--- python-flask-server/docker-entrypoint.sh
+++ python-flask-server/docker-entrypoint.sh
@@ -3,16 +3,17 @@
 if [[ "$1" = 'api' ]]; then
     # update connexion.ini file
     cp ini/connexion-template.ini ini/connexion.ini
-    # update swagger.yaml file
 
-    # install requirements.txt
+    # update swagger.yaml file
+    sed -i 's/host.*/host: \"'${API_SERVER_HOST}':'${API_SERVER_PORT}'\"/g' /datatranslator/swagger/swagger.yaml
 
     # update /etc/hosts if BACKEND_IP is passed in
-    useradd -d /var/pgadmin --system -s /usr/sbin/nologin -c "pgAdmin System Account" -U pgadmin
-    usermod -u ${PGADMIN_UID} pgadmin
-    groupmod -g ${PGADMIN_GID} pgadmin
-    chown -R ${PGADMIN_UID}:${PGADMIN_GID} /pgadmin4 /var/pgadmin
-    su --shell /bin/bash -c "python /pgadmin4/web/pgAdmin4.py" pgadmin
+    if [[ -n $POSTGRES_BACKEND_IP ]]; then
+        echo "${POSTGRES_BACKEND_IP} backend" >> /etc/hosts
+    fi
+
+    # run the app
+    python app.py
 else
     exec "$@"
 fi
