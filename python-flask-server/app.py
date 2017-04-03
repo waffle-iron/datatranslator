949cc9302d0fbc5b6421794209240a89b0250237
Michael J. Stealey <michael.j.stealey@gmail.com>
2017-04-03 14:42:38 -0400

    add ssl support

--- python-flask-server/app.py
+++ python-flask-server/app.py
@@ -2,10 +2,16 @@
 
 import connexion
 from configparser import ConfigParser
+from gevent import monkey
+monkey.patch_all()
 
 if __name__ == '__main__':
     parser = ConfigParser()
     parser.read('ini/connexion.ini')
     app = connexion.App(__name__, specification_dir='./swagger/', server=parser.get('connexion', 'server'))
     app.add_api('swagger.yaml', arguments={'title': 'Environmental Exposures API'})
-    app.run(port=int(parser.get('connexion', 'port')), debug=parser.get('connexion', 'debug'))
+    app.run(port=int(parser.get('connexion', 'port')),
+            debug=parser.get('connexion', 'debug'),
+            keyfile=parser.get('connexion', 'keyfile'),
+            certfile=parser.get('connexion', 'certfile')
+            )
