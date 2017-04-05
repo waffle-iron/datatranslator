c1a98916ea0d1ebc2fae65b6d5f972501660ae2c
Michael J. Stealey <michael.j.stealey@gmail.com>
2017-04-05 11:22:08 -0400

    CORS fix, ssl fix

--- python-flask-server/app.py
+++ python-flask-server/app.py
@@ -11,9 +11,13 @@ if __name__ == '__main__':
     parser.read('ini/connexion.ini')
     app = connexion.App(__name__, specification_dir='./swagger/', server=parser.get('connexion', 'server'))
     app.add_api('swagger.yaml', arguments={'title': 'Environmental Exposures API'})
-    CORS(app)
-    app.run(port=int(parser.get('connexion', 'port')),
-            debug=parser.get('connexion', 'debug'),
-            keyfile=parser.get('connexion', 'keyfile'),
-            certfile=parser.get('connexion', 'certfile')
-            )
+    if not parser.get('connexion', 'certfile') or not parser.get('connexion', 'keyfile'):
+        app.run(port=int(parser.get('connexion', 'port')),
+                debug=parser.get('connexion', 'debug'))
+    else:
+        CORS(app.app)
+        app.run(port=int(parser.get('connexion', 'port')),
+                debug=parser.get('connexion', 'debug'),
+                keyfile=parser.get('connexion', 'keyfile'),
+                certfile=parser.get('connexion', 'certfile')
+                )
